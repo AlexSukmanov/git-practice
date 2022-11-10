aae2e20d94a8ff0b0448461b8f868d482178d9a2
AlexS <alex.sukmanov@nextsilicon.com>
2022-11-10 10:16:43 +0200

    counter.sv added

---
+++ rtl/counter.sv
@@ -0,0 +1,19 @@
+module counter #(
+  parameter int COUNT_NUM = 10000
+) (
+  input logic clk,
+  input logic rstn,
+  output logic counted
+);
+
+  logic [$clog2(COUNT_NUM)-1:0] cnt;
+  always_ff @(posedge clk) begin
+    if (!rstn) begin
+      cnt <= '0;
+    end else begin
+      cnt <= cnt + 1;
+    end
+  end
+
+  assign counted = (cnt == COUNT_NUM) ? 1 : 0; 
+endmodule
